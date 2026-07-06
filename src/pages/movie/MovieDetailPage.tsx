84011fea82b36b591492869938f45d474d86b861
yoonsojung <mila030403@hansung.ac.kr>
2026-07-06 17:54:37 +0900

    feat: 영화 파일 생성

---
+++ src/pages/movie/MovieDetailPage.tsx
@@ -0,0 +1,7 @@
+import AppLayout from '../../components/layout/AppLayout';
+
+const MovieDetailPage = () => {
+  return <AppLayout>MovieDetailPage</AppLayout>;
+};
+
+export default MovieDetailPage;
